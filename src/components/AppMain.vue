2aa584a289db69dc3ff10edfa4f990e0c6d25bca
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-11-09 15:40:25 +0100

    Add CurrentSeriesCard to AppMain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,14 +1,100 @@
 <script>
+import CurrentSeriesCardVue from './CurrentSeriesCard.vue';
 export default{
     name: "AppMain",
-    
+    components:{
+      CurrentSeriesCardVue
+    },
+    data(){
+      return{
+        currentSeries: [
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/AC1000_DLX_162-001_HD_5ba13723281ab0.37845353.jpg?itok=ZsI-C5eX",
+            "price": "$19.99",
+            "series": "Action Comics",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/AV1976_01_300-001_HD_5f738f6e39ddd7.18205602.jpg?itok=VgdYdJ01",
+            "price": "$3.99",
+            "series": "American Vampire 1976",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2018/08/AQMv4_UW_153-001_HD_5b7efa1745a9b8.08589135.jpg?itok=HPTtnrsb",
+            "price": "$16.99",
+            "series": "Aquaman",
+            "type": "graphic novel"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/06/batgirl_v5_1_5b23e1f1124941.50950783.jpg?itok=VnrJah76",
+            "price": "$2.99",
+            "series": "Batgirl",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+            "price": "$3.99",
+            "series": "Batman",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2018/09/BM_56_300-001_HD_5ba137a85c3bf3.33122291.jpg?itok=3FHJQYJZ",
+            "price": "$2.99",
+            "series": "Batman Beyond",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2019/08/BMSM_5d4361b7116261.74371456.jpg?itok=kEDR2OL8",
+            "price": "$3.99",
+            "series": "Batman/Superman",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMSMANN_01_300-001_HD_5f5ff17fa1d665.74704970.jpg?itok=azz5sfGk",
+            "price": "$4.99",
+            "series": "Batman/Superman Annual",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/09/BMTJWZ_01_300-001_HD_5f5ff2307dcb37.34652945.jpg?itok=VswVjLR8",
+            "price": "$5.99",
+            "series": "Batman: The Joker War Zone",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/08/cover-v2_5f40314645a734.37285065.png?itok=ImNM2QBY",
+            "price": "$6.99",
+            "series": "Batman: Three Jokers",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/comic-covers/2020/10/BMWK_HQ_01_300-001_HD_5f7cb4945e13f6.89501032.jpg?itok=sVwALbUX",
+            "price": "$4.99",
+            "series": "Batman: White Knight Presents: Harley Quinn",
+            "type": "comic book"
+          },
+          {
+            "thumb": "https://www.dccomics.com/sites/default/files/styles/covers192x291/public/gn-covers/2019/04/CTWv1_CC_144-001_HD_5ca5299a751963.53054221.jpg?itok=ooPaoLDq",
+            "price": "$16.99",
+            "series": "Catwoman",
+            "type": "graphic novel"
+          }
+        ]
+      }
+    }
+
 }
 </script>
 
 <template>
   <main>
     <div class="container">
-        <h2>Content goes here </h2>
+        <div class="label">CURRENT SERIES</div>
+        <div class="cards">
+            <CurrentSeriesCardVue v-for="(card, index) in currentSeries" :key="index" :thumb="card.thumb" :title="card.series"/>
+        </div>
+        <button>LOAD MORE</button>
     </div>
   </main>
 </template>
@@ -22,6 +108,38 @@ main{
     background-color: $main-black;
     color: white;
     padding: 2rem 0;
+}
+
+.container{
+  position: relative;
+  padding-top: 2rem;
+  .label{
+    background-color: $primary-color;
+    padding: .5rem 1rem;
+    font-size: 1.3rem;
+    font-weight: 600;
+    position: absolute;
+    top: -3.5rem;
+  }
+
+  .cards{
+    display: flex;
+    width: 100%;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 1.5rem;
+  }
+
+  button{
+    background-color: $primary-color;
+    border: 0;
+    padding: 0.8rem 0;
+    width: 200px;
+    color: white;
+    font-weight: 600;
+    margin: 2rem 50% 0;
+    transform: translate(-50%);
+  }
 }
 
 </style>
---
+++ src/components/CurrentSeriesCard.vue
@@ -0,0 +1,44 @@
+<script>
+export default{
+    name: "CurrentSeriesCard",
+    props: {
+        title: String,
+        thumb: String,
+    }
+}
+</script>
+
+<template>
+    <div class="card">
+        <div class="img-wrapper">
+            <img class="thumb" :src="thumb" :alt="title">
+        </div>
+        <p>{{title.toUpperCase()}}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+// links
+@use "../style/general.scss" as *;
+@use "../style/partials/variables" as *;
+// /links
+.card{
+    width: calc(100% / 6 - 1.5rem);
+    p{
+        max-width: 100%;
+        padding-top: 1rem;
+        font-size: .8rem;
+    }
+    .img-wrapper{
+        width: 100%;
+        height: 250px;
+        .thumb{
+            height: 100%;
+            width: 100%;
+            object-fit: cover;
+            object-position: top;
+        }
+    }
+}
+
+</style>
