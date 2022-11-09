e3ebb2c5219f16ffce2273e2ab51780d6984c38f
Alessandro Sartor <alessandro.sartor.1999@gmail.com>
2022-11-09 08:45:16 +0100

    Added top footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,126 @@
 <script>
 export default{
     name: "AppFooter",
+    data(){
+        return{
+            dcComicsLinks: [
+                {
+                    title: "Characters",
+                    href: "/characters",
+                },
+                {
+                    title: "Comics",
+                    href: "/comics",
+                },
+                {
+                    title: "Movies",
+                    href: "/movies",
+                },
+                {
+                    title: "TV",
+                    href: "/tv",
+                },
+                {
+                    title: "Games",
+                    href: "/games",
+                },
+                {
+                    title: "Collectibles",
+                    href: "/collectibles",
+                },
+                {
+                    title: "Videos",
+                    href: "/videos",
+                },
+                {
+                    title: "Fans",
+                    href: "/fans",
+                },
+                {
+                    title: "News",
+                    href: "/news",
+                }
+            ],
+            shopLinks: [
+                {
+                    title: "Shop DC",
+                    href: "/shop"
+                },
+                {
+                    title: "Shop DC Collectibles",
+                    href: "/collectibles",
+                }
+            ],
+            dcLinks: [
+                {
+                    title: "Terms of use",
+                    href: "/terms of use",
+                },
+                {
+                    title: "Privacy policy (new)",
+                    href: "/privacypolicy",
+                },
+                {
+                    title: "Ad choices",
+                    href: "/adchoices",
+                },
+                {
+                    title: "Advertising",
+                    href: "/advertising",
+                },
+                {
+                    title: "Jobs",
+                    href: "/jobs",
+                },
+                {
+                    title: "Subscriptions",
+                    href: "/subscriptions",
+                },
+                {
+                    title: "Talent workshops",
+                    href: "/talentworkshops",
+                },
+                {
+                    title: "CPSC Certificates",
+                    href: "/certificates",
+                },
+                {
+                    title: "Ratings",
+                    href: "/ratings",
+                },
+                {
+                    title: "Shop help",
+                    href: "/shophelp",
+                },
+                {
+                    title: "Contact us",
+                    href: "/contactus",
+                }
+            ],
+            sitesLinks:[
+                {
+                    title: "DC",
+                    href: "/dc",
+                },
+                {
+                    title: "MAD Magazine",
+                    href: "/madmagazine",
+                },
+                {
+                    title: "DC Kids",
+                    href: "/dckids",
+                },
+                {
+                    title: "DC Universe",
+                    href: "/dcuniverse",
+                },
+                {
+                    title: "DC Power Visa",
+                    href: "/dcpowervisa",
+                },
+            ]
+        }
+    }
 }
 
 </script>
@@ -9,9 +129,47 @@ export default{
   <footer>
     <div class="top-footer">
         <div class="container">
-            <div class="col">
-                
+
+            <div class="cols">
+                <div class="col">
+                    <h3>DC COMICS</h3>
+                    <ul class="links">
+                        <li v-for="(link, index) in dcComicsLinks" :key="index" class="link">
+                            <a @click.prevent :href="link.href">{{link.title}}</a>
+                        </li>
+                    </ul>
+                    <h3>SHOP</h3>
+                    <ul class="links">
+                        <li v-for="(link, index) in shopLinks" :key="index" class="link">
+                            <a @click.prevent :href="link.href">{{link.title}}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h3>DC</h3>
+                    <ul class="links">
+                        <li v-for="(link, index) in dcLinks" :key="index" class="link">
+                            <a @click.prevent :href="link.href">{{link.title}}</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col">
+                    <h3>SITES</h3>
+                    <ul class="links">
+                        <li v-for="(link, index) in sitesLinks" :key="index" class="link">
+                            <a @click.prevent :href="link.href">{{link.title}}</a>
+                        </li>
+                    </ul>
+                </div>
+
             </div>
+
+            <div class="logo">                
+                    <img src="../assets/img/dc-logo-bg.png" alt="">
+            </div>
+
         </div>
     </div>
     <div class="bot-footer">
@@ -30,6 +188,46 @@ export default{
 footer{
     .top-footer{
         background-image: url(../assets/img/footer-bg.jpg);
+        color: white;
+        
+        .container{
+            display: flex;
+            justify-content: space-between;
+            padding: 2.5rem 0;
+            .cols{
+                display: flex;
+                justify-content: flex-start;
+                .col{
+                    padding-right: 2rem;
+                    h3{
+                        padding-bottom: .7rem;
+                    }
+                    ul{
+                        padding-bottom: .7rem;
+                        li{
+                            a{
+                                color: white;
+                                opacity: 0.3;
+                                font-size: .9rem;
+                                &:hover{
+                                    opacity: 0.8;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            .logo{
+                position: relative;
+                height: 100%;
+                width: 500px;
+                img{
+                    position: absolute;
+                    top: -4rem;
+                }
+            }
+            
+        }
         
     }
 }
